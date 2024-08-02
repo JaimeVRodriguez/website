298bf6484a5c5d94bfc423b488ae962c329f781a
Jaime Rodriguez <jaime.v.rodriguez.mil@army.mil>
2024-08-02 13:31:45 -0500

    rename portfolio components to project, complete new design of portfolio section

---
+++ src/components/portfolio/ProjectCard.tsx
@@ -0,0 +1,27 @@
+interface PortfolioCardProps {
+    title: string;
+    image: string;
+    description: string;
+    projectLink?: string;
+    repoLink: string;
+    language: string;
+}
+
+export default function ProjectCard({title, image, description, projectLink, repoLink, language}: PortfolioCardProps) {
+    return (
+        <div className="project-card">
+            <img src={image} alt={title} className="project-image"/>
+            <div className="project-title">{title}</div>
+            <div className="project-overlay">
+                <div className="project-details">
+                    <h6>{language}</h6>
+                    <p>{description}</p>
+                    <div className="project-links">
+                        {projectLink && <a href={projectLink} className="cta">View Project</a>}
+                        <a href={repoLink} className="cta">View Code</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/portfolio/ProjectData.ts
@@ -0,0 +1,26 @@
+import {Project} from "./Project";
+
+export const projects: Project[] = [
+    {
+        title: "Coffee Cafe Chatbot",
+        image: "images/coffeebot_mock.png",
+        description: "A console application that allows users to order multiple coffee drinks. Built to scale with a cafe's menu and options.",
+        projectLink: "https://repl.it/@JaimeVRodriguez/CoffeeChatbot#.replit",
+        repoLink: "https://github.com/JaimeVRodriguez/Coffee_Chatbot",
+        language: "Python"
+    },
+    {
+        title: "ATS Hacker",
+        image: "images/ats_mock.jpg",
+        description: "A job posting keyword counter with command line and web interfaces. Maximize your application's chances by ensuring the most used words from the job posting are in your resume.",
+        repoLink: "https://github.com/JaZa-Programming/ATS-Hacker",
+        language: "Python"
+    },
+    {
+        title: "Special Operations Recruiting Data Analysis",
+        image: "images/dai_mock.png",
+        description: "Analyzes Special Operations selection data for Special Forces and Psychological Operations. Includes a predictive model for selection results based on candidate attributes.",
+        repoLink: "https://github.com/JaimeVRodriguez/sof_selection_analysis",
+        language: "Python"
+    }
+]
